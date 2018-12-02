021d74f74d39c17a947e628250aafaa304a3c8f6
Bina Kim <binakim1009@gmail.com>
2018-12-02 11:12:45 -0800

    Complete about page

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -24,36 +24,42 @@ export default () => {
     </div>
     <div className='bio'>
       <div className='col-12 col-lg-6'>
-        <span className='section'>EXPERIENCE</span>
-        <div className='exp'>
-          <div className='title'><span className='company'>Hyundai Card</span> Senior Product Designer</div>
-          <div className='period'>July 2014 - May 2017</div>
-          <div className='description'>Conducted Fintech product strategies, designed responsive websites and mobile app.</div>
-        </div>
-        <div className='exp'>
-          <div className='title'><span className='company'>Korea Telecom</span> Senior Product Designer</div>
-          <div className='period'>April 2012 - June 2014</div>
-          <div className='description'>Designed a camera app and a music streaming app.</div>
-        </div>
-        <div className='exp'>
-          <div className='title'><span className='company'>SK Communications</span> Product Designer</div>
-          <div className='period'>January 2006 - December 2010</div>
-          <div className='description'>Developed the largest social nework service in South Korea. Designed photo editor</div>
+        <div className='sectionCol'>
+          <span className='section'>EXPERIENCE</span>
+          <div className='exp'>
+            <div className='title'><span className='company'>Hyundai Card</span> Senior Product Designer</div>
+            <div className='period'>July 2014 - May 2017</div>
+            <div className='description'>Conducted Fintech product strategies, designed responsive websites and mobile app.</div>
+          </div>
+          <div className='exp'>
+            <div className='title'><span className='company'>Korea Telecom</span> Senior Product Designer</div>
+            <div className='period'>April 2012 - June 2014</div>
+            <div className='description'>Designed a camera app and a music streaming app.</div>
+          </div>
+          <div className='exp'>
+            <div className='title'><span className='company'>SK Communications</span> Product Designer</div>
+            <div className='period'>January 2006 - December 2010</div>
+            <div className='description'>Developed the largest social nework service in South Korea. Designed photo editor</div>
+          </div>
         </div>
       </div>
       <div className='col-12 col-lg-6'>
-        <span className='section'>EDUCATION</span>
-        <div className='exp'>
-          <div className='title'><span className='company'>Academy of Art University</span> San Francisco, CA</div>
-          <div className='description'>Master of Arts / Web Design & New Media (Fall 2018)</div>
+        <div className='sectionCol'>
+          <span className='section'>EDUCATION</span>
+          <div className='exp'>
+            <div className='title'><span className='company'>Academy of Art University</span> San Francisco, CA</div>
+            <div className='description'>Master of Arts / Web Design & New Media (Fall 2018)</div>
+          </div>
+          <div className='exp'>
+            <div className='title'><span className='company'>Hanyang University</span> Seoul, Korea</div>
+            <div className='description'>Bachelor of Fine Arts / Visual Design (Spring 2006)</div>
+          </div>
         </div>
-        <div className='exp'>
-          <div className='title'><span className='company'>Hanyang University</span> Seoul, Korea</div>
-          <div className='description'>Bachelor of Fine Arts / Visual Design (Spring 2006)</div>
-        </div>
-        <span className='section'>SKILLS</span>
-        <div className='exp'>
-          <div className='description'>Sketch, Flinto, Invision, Adobe XD / Illustrator / Photoshop / Indesign / After effect</div>
+        <div className='sectionCol'>
+          <span className='section'>SKILLS</span>
+          <div className='exp'>
+            <div className='description'>Sketch, Flinto, Invision, Adobe XD / Illustrator / Photoshop / Indesign / After effect</div>
+          </div>
         </div>
       </div>
     </div>
