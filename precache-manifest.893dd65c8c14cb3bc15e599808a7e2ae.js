d510a6b6681ad3b9fae8498401633017290546a1
binakim <binakim1009@gmail.com>
2018-12-02 01:13:04 -0800

    Updates

--- precache-manifest.893dd65c8c14cb3bc15e599808a7e2ae.js
+++ precache-manifest.893dd65c8c14cb3bc15e599808a7e2ae.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "d34dedce89a310a4942e",
-    "url": "/portfolio/static/js/main.d34dedce.chunk.js"
+    "revision": "46fe2cb38e291b32d168",
+    "url": "/portfolio/static/js/main.46fe2cb3.chunk.js"
   },
   {
     "revision": "5876d39a78c0d98f9485",
     "url": "/portfolio/static/js/1.5876d39a.chunk.js"
   },
   {
-    "revision": "d34dedce89a310a4942e",
-    "url": "/portfolio/static/css/main.e5b8b72f.chunk.css"
+    "revision": "46fe2cb38e291b32d168",
+    "url": "/portfolio/static/css/main.214e9599.chunk.css"
   },
   {
-    "revision": "aacab249b821932255f5e05ae2b00f57",
+    "revision": "0cadd420c0d80b3e2f70248f0c653b7f",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.bfb85db780011f03db470a268b593ee2.js"
+  "/portfolio/precache-manifest.893dd65c8c14cb3bc15e599808a7e2ae.js"
 );
 
 workbox.clientsClaim();
