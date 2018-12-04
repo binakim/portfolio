dd9a1e16c2d62ef0bee0a03a4f16f4157cae3a97
Bina Kim <binakim1009@gmail.com>
2018-12-03 23:09:12 -0800

    Updates

---
+++ precache-manifest.a6be1c8ec47692f415878503c476fece.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "22c71c46aa38e1e3ec43",
+    "url": "/portfolio/static/js/main.22c71c46.chunk.js"
+  },
+  {
+    "revision": "588182f1327e1f2cc32c",
+    "url": "/portfolio/static/js/1.588182f1.chunk.js"
+  },
+  {
+    "revision": "22c71c46aa38e1e3ec43",
+    "url": "/portfolio/static/css/main.ea73f55d.chunk.css"
+  },
+  {
+    "revision": "a81a7b8587d3976c946b0bc6e7deebaa",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.43be46bbf05d608da5b9b7563c9186e3.js"
+  "/portfolio/precache-manifest.a6be1c8ec47692f415878503c476fece.js"
 );
 
 workbox.clientsClaim();
